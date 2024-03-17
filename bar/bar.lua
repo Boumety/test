3eb53d70fac49a751d3745a80edcffbc14e02c3b
Boumety <162808181+Boumety@users.noreply.github.com>
2024-03-17 11:00:57 +0100

    Update bar.lua

--- bar/bar.lua
+++ bar/bar.lua
@@ -11,31 +11,52 @@ local DEFAULT_CONFIG = {
     min = 0,
     max = 100,
     value = 0,
+	time = 1
 }
 
 local conf = require("config")
 local ui = require("uikit")
+local ease = require("ease")
 
 mod.createBar = function(self, config)
     config = conf:merge(DEFAULT_CONFIG, config)
-
+	config.min = 0
+	
     local bg = ui:createFrame(config.colorBg)
     bg.Width = config.width
     bg.Height = config.height
-
+	bg.pos.X = config.position
+	bg.pos.Y = config.position
+	
     local bar = ui:createFrame(config.colorBar)
     bar.multiplier = config.value / config.max
 
+	bar:setParent(bg)
+	
     if config.value > config.max then config.value = config.max end
     bar.Width = bar.multiplier * config.width - config.margin
     bar.Height = bg.Height - config.margin
-	bar.pos.X = config.position
-	bar.pos.Y = config.position
 
+
+	bg.updateBar = function(value, callback)
+		if type(value) ~= "number" or type(value) ~= "integer" then
+			error("'value' should be a number or an integer")
+		end
+
+	    bar.multiplier = config.value / config.max
+		ease:cancel()
+		local done
+		if type(callback) == "function" then
+			local done.onDone = callback
+		end
+		ease:inSine(bar, config.time, done).Width = bar.multiplier * config.width - config.margin
+	end
+
+	bg.updateConfig = function(newConfig)
+		config = conf:merge(config, newConfig)
+	end
+	
     return bg
-end
-
-mod.updateBar = function(self, bar, value)
 end
 
 return mod
